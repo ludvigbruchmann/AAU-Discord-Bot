d1b3c8803797c72107f1187af2e08ad927d57935
Ludvig Alexander Brüchmann <norskertv@gmail.com>
2018-01-14 19:17:04 +0100

    Limited passphrase message to #verify-me

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ async def on_message(message):
 
         database.addUser(message.author, message.author.id, keyword)
         await client.send_message(
-            message.channel,
+            serverInfo.server.get_channel(config.verifyChannel),
             "%s this is your passphrase: `%s`\nPlease send an email to `aau@ludvig.xyz` from your Aalborg University email adress with the passphrase as the content to get verified. This process can take a few minutes" % (message.author.mention, keyword)
         )
 
