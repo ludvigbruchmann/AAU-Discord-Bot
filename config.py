78a91fb7cfe6972f875c4040fb72a20ba3c64d72
Ludvig Alexander Brüchmann <norskertv@gmail.com>
2018-01-14 18:59:46 +0100

    config file

---
+++ config.py
@@ -0,0 +1,22 @@
+#db
+autosave = True #recommended
+defaultUser = {
+    "verified": False,
+    "passphrase": "",
+    "username": "",
+    "email": "",
+    "id": ""
+}
+passphraseWords = 4
+
+#discord
+token = '#'
+discordServer = '#'
+verified = 'Verified Student'
+verifyChannel = '#'
+
+#mail
+server = '#'
+username = '#'
+password = '#'
+wait = 10 #seconds to wait in between refreshing email
