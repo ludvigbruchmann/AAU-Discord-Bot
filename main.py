32392ede1423d2332a620dbb062ba5f9d510a014
Ludvig Alexander Brüchmann <norskertv@gmail.com>
2018-01-15 14:31:16 +0100

    Optimization and "security" update
    Will attempt to dm users their passphrase and only use verify channel as
    a fallback

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ class ServerInfo: #weird object to circumvent discord.py's blocking run function
     role = False
 serverInfo = ServerInfo()
 
+def cmd(command):
+    return config.commandPrefix + command
+
 async def mailLoop():
     await client.wait_until_ready()
     while not client.is_closed:
@@ -36,20 +39,27 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    elif message.content.startswith('!thonk'):
+    elif message.content.startswith(cmd('thonk')):
         await client.send_message(message.channel, content = "<a:thinkspin:394968623753723905>") #AAU CPH discord animated emoji
 
-    elif message.content.startswith('!verify'):
+    elif message.content.startswith(cmd('verify')):
         keyword = passphrase.generatePassphrase(config.passphraseWords)
 
         while database.passphraseExists(keyword): #if passphrase is already used generate a new passphrase
             keyword = passphrase.generatePassphrase(config.passphraseWords)
 
         database.addUser(message.author, message.author.id, keyword)
-        await client.send_message(
-            serverInfo.server.get_channel(config.verifyChannel),
-            "%s this is your passphrase: `%s`\nPlease send an email to `aau@ludvig.xyz` from your Aalborg University email adress with the passphrase as the content to get verified. This process can take a few minutes" % (message.author.mention, keyword)
-        )
+
+        try: #try sending the passphrase to user in a dm
+            await client.send_message(
+                message.author,
+                "%s this is your passphrase: `%s`\nPlease send an email to `%s` from your Aalborg University email adress with the passphrase as the content to get verified. This process can take a few minutes" % (message.author.mention, keyword, config.username)
+            )
+        except discord.errors.Forbidden as e: #If user does not allow dm's from server members we will send the passphrase in the verify channel instead
+            await client.send_message(
+                serverInfo.server.get_channel(config.verifyChannel),
+                "%s this is your passphrase: `%s`\nPlease send an email to `%s` from your Aalborg University email adress with the passphrase as the content to get verified. This process can take a few minutes" % (message.author.mention, keyword, config.username)
+            )
 
 client.loop.create_task(mailLoop())
 client.run(config.token)
